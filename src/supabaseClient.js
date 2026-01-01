f51cc1bd00d0fd0b3a162db90df19f6881f5067c
crofteric40-boop <crofteric40@gmail.com>
2026-01-01 14:30:26 -0700

    Fix Supabase API key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://qjbfkhpvfmpqchwfbino.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqYmZraHB2Zm1wcWNod2ZiaW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU3NjAyNjcsImV4cCI6MjA1MTMzNjI2N30.Gq-fBBaZzMb-PfaLlT5WgQ_RG83iu8q'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqYmZraHB2Zm1wcWNod2ZiaW5vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyNjI4ODksImV4cCI6MjA4MjgzODg4OX0.b-Dw12fvmsl0lUFPZOOA9XMd6soLU3ToDO8l07Fqe6E'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
